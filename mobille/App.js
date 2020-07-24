f1de776d1c605118ae13947341b950e8b3b62d46
Rafoshi <58670715+Rafoshi@users.noreply.github.com>
2020-07-24 16:43:54 -0300

    Almost Finished

---
+++ mobille/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View>
+      <Text>Hello</Text>
+    </View>
+  );
+}
+
---
+++ mobille/src/pages/incidents/index.js
@@ -0,0 +1,10 @@
+import react from 'react';
+import {view} from 'react-native';
+
+export default function Incidents (){
+    return(
+        <view>
+            
+        </view>
+    );
+}
